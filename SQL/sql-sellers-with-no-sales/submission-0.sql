3bb046d4cc6690270603c585a145e0f973a6d2aa
Ayush Rajput <2301641550052@aktuacin.onmicrosoft.com>
2026-07-28 16:20:13 +0530

    Add: sql-sellers-with-no-sales - submission-0

---
+++ SQL/sql-sellers-with-no-sales/submission-0.sql
@@ -0,0 +1,10 @@
+-- Write your query below
+SELECT
+    s.seller_name
+FROM 
+    seller s
+LEFT JOIN orders o
+    ON o.seller_id = s.seller_id 
+    AND EXTRACT(YEAR FROM o.sale_date) = 2020
+WHERE o.order_id IS NULL
+ORDER BY s.seller_name ASC;
